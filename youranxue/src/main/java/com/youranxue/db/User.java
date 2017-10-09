46d8ae8db8566f10dd316a21ab1190a6b83cec7e
ionicview <ionicview@163.com>
2017-10-09 21:54:01 +0900

    add image controller

--- youranxue/src/main/java/com/youranxue/db/User.java
+++ youranxue/src/main/java/com/youranxue/db/User.java
@@ -2,6 +2,9 @@ package com.youranxue.db;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import lombok.Data;
+
+@Data
 public class User {
 
 	private String name;
@@ -12,37 +15,9 @@ public class User {
 
 	private String password;
 
-	public String getName() {
-		return this.name;
-	}
+	private String firstName;
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getUsername() {
-		return this.username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getEmail() {
-		return this.email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return this.password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
+	private String lastName;
 
 	public void encodePassword(PasswordEncoder passwordEncoder) {
 		this.password = passwordEncoder.encode(this.password);
